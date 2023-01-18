9a03634ebdcfaa2cee38fe540001e0fa2657ccfd
Gustavo Matos Lázaro <gustavolazaro@alunos.utfpr.edu.br>
2023-01-18 17:54:34 -0300

    Update Base64FileViceVersa.dart

--- Base64FileViceVersa.dart
+++ Base64FileViceVersa.dart
@@ -1,36 +1,42 @@
 import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
+import 'package:flutter/cupertino.dart';
 import 'package:path_provider/path_provider.dart'; //https://pub.dev/packages/path_provider/install
 
-class Base64FileViceVersa {
+class DartUtils {
 
-    static Future<String> createPngFileFromString(String encodedStr) async {
+  static Future<String?> createFileFromString({required String base64String, required String fileExtension}) async {
     try{
-      Uint8List bytes = base64.decode(encodedStr);
+      Uint8List bytes = base64.decode(base64String);
       String dir = (await getApplicationDocumentsDirectory()).path;
-      File file = File("$dir/${DateTime.now().millisecondsSinceEpoch}.png");
+
+      if(fileExtension.contains('.')){
+        fileExtension.replaceAll('.', '');
+      }
+
+      File file = File("$dir/${DateTime.now().millisecondsSinceEpoch}.$fileExtension");
       await file.writeAsBytes(bytes);
       return file.path;
     } catch (error){
-      debugPrint('error createPngFileFromString -> ${error.toString()}');
+      debugPrint('error createFileFromString -> ${error.toString()}');
       return null;
     }
   }
 
-  static Future<String> getBase64FromFIle(String filePath) async {
+  static Future<String?> getBase64FromFile(String filePath) async {
     try{
       File aux = File.fromUri(Uri.parse(filePath));
-      Uint8List bytesFile;
+      Uint8List? bytesFile;
       await aux.readAsBytes().then((value) {
         bytesFile = Uint8List.fromList(value);
         debugPrint('reading of bytes is completed');
       }).catchError((onError) {
-        debugPrint('Exception Error while reading audio from path: $onError');
+        debugPrint('Exception Error while reading file from path: $onError');
       });
-      return base64Encode(bytesFile);
+      return base64Encode(bytesFile!);
     } catch (error){
-      debugPrint('error getBase64FromFIle -> ${error.toString()}');
+      debugPrint('error getBase64FromFile -> ${error.toString()}');
       return null;
     }
   }
